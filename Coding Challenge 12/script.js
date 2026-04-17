49221678c2ada812759fd0692d606408d821d2d1
Hanjun Cho <89818277+Jukdori@users.noreply.github.com>
2026-04-17 14:37:44 +0000

    commit all of the changes

--- Coding Challenge 12/script.js
+++ Coding Challenge 12/script.js
@@ -13,10 +13,7 @@ function updateElement(selector, text, color) {
     element.style.color = color;
 }
 
-// 기존 예제 실행
 changeText("#greeting", "Welcome to class!");
 changeText("#counter", "Count: 42");
 changeHTML("#bio", "My name is <strong>Hanjun</strong> and I study <em>Communication design</em>.");
-
-// 과제 실행
 updateElement("#greeting", "Hello <strong>again</strong>!", "blue");
